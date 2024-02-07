8be6d461dc458ecab3e2d4107943d68ea665267b
Shaxzod <sanedidi98@gmail.com>
2024-02-07 18:36:10 +0500

    gg

--- src/Components/Group/Group.jsx
+++ src/Components/Group/Group.jsx
@@ -9,7 +9,6 @@ import Pba from "../../assets/PBAbiznes.jpg";
 import sss from "../../assets/sss.jpg";
 import anor from "../../assets/logo3.png";
 
-// import required modules
 import { Autoplay, Pagination } from "swiper/modules";
 import "./Group.scss";
 const Group = () => {
@@ -18,43 +17,90 @@ const Group = () => {
       <div className="group__wrapper">
         <div className="group__title">
           <h2>Group Of Companies</h2>
-          <Swiper
-            spaceBetween={10}
-            centeredSlides={true}
-            slidesPerView={3}
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            loop={true}
-
-            navigation={true}
-            modules={[Autoplay, Pagination]}
-            className="mySwiper"
-          >
-            <SwiperSlide className="swiper">
-              <img src={mnSoft} alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="swiper">
-              <img src={mnSolutions} alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="swiper">
-              <img src={sekuit} alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="swiper">
-              <img src={Navin} alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="swiper">
-              <img src={Pba} alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="swiper">
-              <img src={sss} alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="swiper">
-              <img src={anor} alt="" />
-            </SwiperSlide>
-          </Swiper>
         </div>
+        <Swiper
+          centeredSlides={true}
+          slidesPerView={3}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          navigation={true}
+          modules={[Autoplay, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://mn-soft.uz/">
+              <img src={mnSoft} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="">
+              <img src={mnSolutions} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://sekuit.uz/">
+              <img src={sekuit} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://navin.uz/">
+              <img src={Navin} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://pbabiznes.uz/">
+              <img src={Pba} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://en-gy.ru/">
+              <img src={sss} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a href="https://anorlevel.uz/">
+              <img src={anor} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://mn-soft.uz/">
+              <img src={mnSoft} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://mn-solutions.uz/">
+              <img src={mnSolutions} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://sekuit.uz/">
+              <img src={sekuit} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://navin.uz/">
+              <img src={Navin} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://pbabiznes.uz/">
+              <img src={Pba} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://en-gy.ru/">
+              <img src={sss} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="swiper">
+            <a target="_blank" href="https://anorlevel.uz/">
+              <img src={anor} alt="" />
+            </a>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   );
